7d303d03d3be4650c652981b8932f0fd6ac57fc9
Ashna <aa57c@umsystem.edu>
2023-02-06 17:14:05 -0600

    added firebase config. connected database. tested it. it works!

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCh2X2opN14ARAoka-hMeVvPzrYoQ_UCwk",
+  authDomain: "capstone-proj-da7cf.firebaseapp.com",
+  projectId: "capstone-proj-da7cf",
+  storageBucket: "capstone-proj-da7cf.appspot.com",
+  messagingSenderId: "914250843410",
+  appId: "1:914250843410:web:4dbcbc435244006646dfcb",
+  measurementId: "G-645V4K0WCS"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
