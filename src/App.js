5734eb1933f80c309dee6bdd2de69111bcd0aa4b
Devin Osbern <drosbern@gmail.com>
2023-02-07 21:02:34 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {db, storage} from './firebase-config';
 import {collection, getDocs, addDoc, updateDoc, doc, deleteDoc} from 'firebase/firestore';
 import {ref, uploadBytes} from 'firebase/storage';
 import {v4} from 'uuid'
+import NavBar from "./NavBar";
 
 
 /*
@@ -80,6 +81,9 @@ function App() {
 
   return (
     <div className="App">
+      <div>
+        <NavBar />
+      </div>
       <input placeholder="Name..." 
       onChange={(event) => {setNewName(event.target.value)}}/>
       <input type="number" 
@@ -108,7 +112,6 @@ function App() {
         <button onClick={uploadFile}>Upload File</button> 
 
       </div>
-     
 
       {
         /*
