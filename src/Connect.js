5b835e63a3a6285923be80b449b4ee01e7c049be
srsf35 <77651825+srsf35@users.noreply.github.com>
2023-02-23 20:59:40 -0600

    Now lists all classes and section ID in Classes Collection

--- src/Connect.js
+++ src/Connect.js
@@ -3,29 +3,38 @@ const {MongoClient} = require('mongodb');
 
 
 async function main(){
-    /**
+   listClasses();
+}
+
+async function listClasses()
+{
+     /**
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://srsf35:123@cluster0.ie1muht.mongodb.net/?retryWrites=true&w=majority";
+     const uri = "mongodb+srv://srsf35:123@cluster0.ie1muht.mongodb.net/?retryWrites=true&w=majority";
  
 
-    const client = new MongoClient(uri);
+     const client = new MongoClient(uri);
+  
+     try {
+         // Connect to the MongoDB cluster
+         await client.connect();
+  
+         // Make the appropriate DB calls
+         const databasesList = await client.db('mydb').collection('Classes').find({});
  
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
  
-        // Make the appropriate DB calls
-        const databasesList = await client.db().admin().listDatabases();
-
-        console.log(databasesList);
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+         await databasesList.forEach((result, i) => {
+             console.log();
+             console.log(`${i + 1}. name: ${result.className}`);
+             console.log(`sectionID: ${result.sectionID}`);
+         });
+     } catch (e) {
+         console.error(e);
+     } finally {
+         await client.close();
+     }
 }
 
 main().catch(console.error);
