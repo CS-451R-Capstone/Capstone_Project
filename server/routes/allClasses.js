204ad19470ece774b292836d52d58a0e0846680a
aa57c <aa57c@umsystem.edu>
2023-03-03 12:33:58 -0600

    removed connection file from front-end code and implemented business logic in backend

--- server/routes/allClasses.js
+++ server/routes/allClasses.js
@@ -8,14 +8,15 @@ const recordRoutes = express.Router();
 // This will help us connect to the database
 const dbo = require("../db/connection");
 
-recordRoutes.route('/allClasses').get((req, res) => {
+recordRoutes.route('/').get((req, res) => {
     let connect = dbo.connectToServer();
     connect.then(db => {
-        db.collection('Classes').find({}).toArray((err, result) =>{
+        db.collection('Classes').aggregate([{$project: {className: 1, sectionID: 1}}]).toArray((err, result) =>{
             if(err){
                 throw err;
             }
             res.json(result);
+            
         });
     }).finally(() =>{
         let close = dbo.closeServer();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,6 @@
 import '../App.css';
 import NavBar from "../navigation/NavBar";
-import { useEffect } from 'react';
-import listClasses from '../Connect';
 function Home() {
- useEffect(()=> {
-  //handle the promise.
-  const promise = listClasses();
-  promise.then(() => {
-    console.log(promise);
-  })
- });
   return (
     <div className="App">
       <div>
