6480fc9d78996fbc1d998da314ed9ddb2d11ac48
aa57c <aa57c@umsystem.edu>
2023-03-21 17:09:30 -0500

    finished merging changes

--- server/routes/dbCalls.js
+++ server/routes/dbCalls.js
@@ -4,56 +4,13 @@ const express = require("express");
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const recordRoutes = express.Router();
-<<<<<<< HEAD
-=======
 const upload = require('express-fileupload');
->>>>>>> MongoDBConnection
  
 // This will help us connect to the database
 //
 const dbo = require("../db/connection");
 
 recordRoutes.route('/home').get((req, res) => {
-<<<<<<< HEAD
-    let connect = dbo.connectToServer();
-    connect.then(db => {
-        db.collection('Classes').aggregate([{$project: {_id: 0, className: 1, sectionID: 1}}]).toArray((err, result) =>{
-            if(err){
-                throw err;
-            }
-            res.json(result);
-            
-        });
-    });
-    
-});
-
-recordRoutes.route('/postings').get((req, res) => {
-    let connect = dbo.connectToServer();
-    connect.then(db => {
-        db.collection('Classes').aggregate([{$project: { 
-            _id: 0, 
-            className: 1,
-            postings: 
-            {
-            "posting1.is_GTA_Required": 1, 
-            "posting1.job_title": 1, 
-            "posting2.is_GTA_Required": 1, 
-            "posting2.job_title": 1
-            }
-        }}]).toArray((err, result) => {
-            if(err){
-                throw err;
-            }
-            res.json(result);
-        });
-    })
-    
-    
-    
-});
-
-=======
     
     //Serves the classes on request.
     dbo.getDB().collection('Classes').aggregate([{$project: {_id: 0, className: 1, sectionID: 1}}]).toArray((err, result) =>{
@@ -97,7 +54,6 @@ recordRoutes.route('/applicants').post(upload({createParentPath: true}),
         res.json({status: 'logged', message: 'logged'})
     }
 );
->>>>>>> MongoDBConnection
 
 
 
