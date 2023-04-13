93045591c8751a7b574924a9aaf3748eb72a2657
aa57c <aa57c@umsystem.edu>
2023-04-13 18:56:01 -0500

    added new login page that looks pretty

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,71 +1,128 @@
 import React, {useState, useEffect} from 'react';
 import logo from '../UMKC_Logo.png';
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+//import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          UMKC GTA Job Postings
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+
+    );
+}
+
+const theme = createTheme();
+
 
 function Login(){
 
     const location = useLocation();
+    const history = useHistory();
     const posting = location.state?.posting;
     const className = location.state?.className;
     const sectionID = location.state?.sectionID;
 
-    /*
-
-    const [userLogins, setUserLogins] = useState([]);
-    useEffect(() => {
-        async function getLogins(){
-            const response = await fetch(`http://localhost:5000/logins`);
-            if(response.ok){
-                let userLogins = await response.json();
-                setUserLogins(userLogins);
-                return;
-            }
-        }
-        getLogins();
-        return;
-    }, [userLogins.length]);
-    */
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
 
-    //function Authenticate(){
-        
+    const handleSubmit = () => {
+        const userData = {
+            email: email,
+            password: password
+        };
+        console.log(userData);
+        history.push({
+            pathname: '/submission-portal',
+            state: {
+                posting: posting,
+                className: className,
+                sectionID: sectionID
+            }
+        });
 
-    //}
+    }
 
     return(
-        <div className='sign-in-container'>
-            <div className="logo">
-                <img src={logo} alt="UMKC" />
-            </div>
-            <form onSubmit={""}>
-                <h1>Log In</h1>
-                <input 
-                type="email" 
-                placeholder='Enter your email...' 
-                value={email}
-                onChange={(event) => {setEmail(event.target.value)}}
-                />
-                <input 
-                type="password" 
-                placeholder='Enter your password...' 
-                value={password}
-                onChange={(event) => {setPassword(event.target.value)}}
-                />
-                <Link to={{pathname: '/submission-portal', 
-                    state: {
-                        posting: posting,
-                        className: className,
-                        sectionID: sectionID
-                    }}}>
-                    <button>Log In</button>
-                </Link>
-                
-                
-            </form>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+            <Box
+                sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            error={errors.email}
+            onError={(event) => setErrors({...errors, [event.target.name]: event.target.error})}
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            error={errors.password}
+            onError={(event) => setErrors({...errors, [event.target.name]: event.target.error})}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+            onClick={handleSubmit}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            >
+                Sign In
+            </Button>
+            
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
     )
 }
 export default Login;
